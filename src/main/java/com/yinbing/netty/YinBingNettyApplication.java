d1231d5bd563b1e1bdc8eee57eea3c79d882c766
jixiang.wang <jixiang.wang@openjawtech.com>
2020-07-02 14:51:51 +0800

    Initial commit

---
+++ src/main/java/com/yinbing/netty/YinBingNettyApplication.java
@@ -0,0 +1,13 @@
+package com.yinbing.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinBingNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YinBingNettyApplication.class, args);
+	}
+
+}
